f51775464d8bbe229a1d8dc5efc3905e733b7ed2
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2010-06-28 09:33:58 -0500

    Deleted incompleted VB demo.

--- Silverlight/UpdateControls.Light.Demo.VB/Invoice.vb
+++
@@ -1,6 +0,0 @@
-﻿Public Class Invoice
-    Public Sub New(ByVal number As String)
-        Throw New NotImplementedException()
-    End Sub
-
-End Class
--- Silverlight/UpdateControls.Light.Demo.VB/PaymentNavigationModel.vb
+++
@@ -1,3 +0,0 @@
-﻿Public Class PaymentNavigationModel
-
-End Class
