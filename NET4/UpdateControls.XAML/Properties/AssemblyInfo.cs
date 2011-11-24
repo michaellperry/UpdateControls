e5842f7e65e931b1ab8990dfc5396d653bf89027
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2011-11-24 12:21:43 -0600

    Added Mango and .NET 4 builds.

---
+++ NET4/UpdateControls.XAML/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls.XAML")]
+[assembly: AssemblyProduct("UpdateControls.XAML")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("76db5e5e-92c6-420c-8d11-0118bb319689")]
---
+++ NET4/UpdateControls/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UpdateControls")]
+[assembly: AssemblyProduct("UpdateControls")]
+[assembly: CLSCompliant(true)]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d1905384-b468-44c5-97de-8cfc05531bfd")]
