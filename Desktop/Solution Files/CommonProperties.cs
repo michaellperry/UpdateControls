698f311410f7ef4f55db643ae9f73682dd2508e6
MichaelLPerry1971_cp <SND\MichaelLPerry1971_cp>
2012-10-05 17:00:27 -0500

    Version 2.2.1.4.

--- Desktop/Solution Files/CommonProperties.cs
+++ Desktop/Solution Files/CommonProperties.cs
@@ -18,5 +18,5 @@ using System;
 [assembly: AssemblyCopyright("Copyright 2012 Michael L Perry, MIT License")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("2.2.1.3")]
-[assembly: AssemblyFileVersion("2.2.1.3")]
+[assembly: AssemblyVersion("2.2.1.4")]
+[assembly: AssemblyFileVersion("2.2.1.4")]
