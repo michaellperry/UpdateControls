9056b0d11e4197bebb50866d028a31a329ccdec8
Robert Vazan <robert.vazan@example.com>
2014-06-13 21:54:04 +0200

    Automatic ICommand conversion performed only for void-returning methods, Can* property used only if it returns bool

--- WPF/UpdateControls.XAML/Wrapper/ClassInstance.cs
+++ WPF/UpdateControls.XAML/Wrapper/ClassInstance.cs
@@ -38,8 +38,11 @@ namespace UpdateControls.XAML.Wrapper
                 .ToList();
             _classProperties.AddRange(
                 (from method in _wrappedType.GetMethods()
-                 where method.GetParameters().Length == 0
-                 select new ClassMemberCommand(method, _classProperties.FirstOrDefault(p => p.Name == "Can" + method.Name), objectInstanceType)).ToList());
+                 where method.ReturnType == typeof(void) && method.GetParameters().Length == 0
+                 let can = (from p in _classProperties
+                            where p.Name == "Can" + method.Name && p.PropertyType == typeof(bool)
+                            select p).FirstOrDefault()
+                 select new ClassMemberCommand(method, can, objectInstanceType)).ToList());
             _propertyDescriptors = new PropertyDescriptorCollection(_classProperties.ToArray());
 
             // Create a pass-through for each event.
